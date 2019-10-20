6edb9966e9a8bde0847dd0fe8a93fdf2efa32a58
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-10-20 16:45:23 +0200

    golint and go fmt as part of the build

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+(cd instances-get || exit 1; go fmt)
+(cd instances-get || exit 1; golint)
 (cd instances-get || exit 1; go test)
 (cd instances-get || exit 1; GOOS=linux go build)
 (cd instances-get || exit 1; mv instances-get main)
 (cd instances-get || exit 1; zip cloudstart-backend-instances-get.zip main)
 
+(cd instances-patch || exit 1; go fmt)
+(cd instances-patch || exit 1; golint)
 (cd instances-patch || exit 1; GOOS=linux go build)
 (cd instances-patch || exit 1; mv instances-patch main)
 (cd instances-patch || exit 1; zip cloudstart-backend-instances-patch.zip main)
